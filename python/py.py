ad6eed07aae4fdb510eb0f6f9ecdbf940ccabf7c
Adryel martins <adryeldev@hotmail.com>
2023-02-28 14:30:55 -0300

    firstcommit

---
+++ python/py.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#
+# Seu código
+#
+def reverseString(text):
+    pass
+
+#
+# Seu teste
+#
+palavra = "animal"
+assert "lamina" == reverseString(palavra)
---
+++ start.js
@@ -0,0 +1,10 @@
+function inverter(string){
+    var saida = ''
+    for (var i=string.length-1; i>=0;i--){
+        saida +=string[i]
+    }
+    console.log(saida)
+}
+
+inverter("palavra")
+
